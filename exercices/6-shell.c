df4eb2371c8a555268cf8278cf7a621a91f26f28
FelixBesancon <f.besancon@hotmail.fr>
2025-12-17 08:54:30 +0000

    exercices

---
+++ exercices/6-shell.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+int main(void)
+{
+	int ac;
+	pid_t PID;
+	char *line = NULL, *token = NULL, **av = NULL;
+	size_t buffer = 0;
+	ssize_t len;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		len = getline(&line, &buffer, stdin);
+		if (len > 1)
+		{
+			if (len > 0 && line[len - 1] == '\n')
+				line[len - 1] = '\0';
+			av = malloc((len + 1) * sizeof(char *));
+			if (av == NULL)
+			{
+				free(line);
+				exit(1);
+			}
+			token = strtok(line, " \n\t");
+			for (ac = 0; token != NULL; ac++)
+			{
+				av[ac] = token;
+				token = strtok(NULL, " \n\t");
+			}
+			av[ac] = NULL;
+			if (av[0] != NULL)
+			{
+				PID = fork();
+				if (PID == -1)
+				{
+					perror("./shell");
+					free(av);
+					free(line);
+					return (1);
+				}
+				if (PID == 0)
+				{
+					execve(av[0], av, environ);
+					perror("./shell");
+					exit(1);
+				}
+				else
+					wait(NULL);
+			}
+			free(av);
+		}
+		else if (len == -1)
+		{
+			printf("\n");
+			free(line);
+			exit(0);
+		}
+	}
+	free(line);
+	return (0);
+}
