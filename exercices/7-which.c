3eae89b64903cc8d8234ffc55244c18f7fcfaa0c
FelixBesancon <f.besancon@hotmail.fr>
2025-12-17 13:47:44 +0000

    Add 7-which.c program

--- exercices/7-which.c
+++ exercices/7-which.c
@@ -9,7 +9,7 @@ char **get_path_array(char **path_cpy)
 {
 	char *path = getenv("PATH");
 	char **path_array, *path_token;
-	int index = 0, index_2, path_count = 1;
+	int index = 0, path_count = 1;
 
 	if (path == NULL)
 		exit(1);
@@ -28,7 +28,7 @@ char **get_path_array(char **path_cpy)
 	path_array = malloc((path_count + 1) * sizeof(char *));
 	if (path_array == NULL)
 	{
-		free(path_cpy);
+		free(*path_cpy);
 		exit(1);
 	}
 	path_token = strtok(*path_cpy, ":");
@@ -43,14 +43,26 @@ char **get_path_array(char **path_cpy)
 
 int main(int ac, char **av)
 {
-	char *path_cpy, **path_array;
+	char *path_cpy, **path_array, full_path[1024];
 	struct stat av_stat;
+	int index, stat_return = 1;
 
-	if (ac < 2)
+	if (ac != 2)
+	{
+		printf("Usage: %s path_to_file\n", av[0]);
 		return (1);
+	}
 	path_array = get_path_array(&path_cpy);
-
+	for (index = 0; path_array[index] != NULL && stat_return != 0; index++)
+	{
+		sprintf(full_path, "%s/%s", path_array[index], av[1]);
+		stat_return = stat(full_path, &av_stat);
+	}
+	if (stat_return == 0)
+		printf("Found: %s\n", full_path);
+	else
+		printf("Not Found\n");
 	free(path_array);
-	free(*path_cpy);
+	free(path_cpy);
 	return (0);
 }
